bf27033c320429b1baf79c4e43e799db32635fe0
Zachary Charlop-Powers <zcpowers@Zacharys-MacBook-Air.local>
2022-09-17 13:46:17 -0400

    get a few examples

--- notebooks/explore_nextprot.clj
+++ notebooks/explore_nextprot.clj
@@ -14,12 +14,12 @@
   ;; `:dev` alias, evaluate these two forms, point your browser there,
   ;; then read on. :)
   (clerk/serve! {:watch-paths ["notebooks"]})
-  (clerk/show! "notebooks/explore_nextprot.clj")
+  (clerk/show! "notebooks/explore_nextprot.clj"))
 ;;   (clerk/build-static-app! {:paths ["notebooks/explore_uniprot.clj"
 ;;                                     "notebooks/explore_pubchem.clj"
 ;;                                     "notebooks/explore_chembl.clj"
 ;;                                     "notebooks/explore_metanetx.clj"]})
-  )
+  
 
 
 
@@ -32,23 +32,46 @@
 ;;
 ;; 
 
-;; # NextProte Sparql Examples
+;; # NextProt Sparql Examples
 ;; 
-;; ## Basic Information
-;; Retrieve the MNXref metabolite with name 
-;;  *N,N-dimethyl-beta-alanine*, together with molecular 
-;;  information. 
+;; ## NXQ1 
+;; Phospho-proteins in the cytoplasm
+
 (sq/query
  :nextprot
- `{:select-distinct [?entry] 
+ `{:select-distinct [?entry]
    :where [
-           ;; Todo: this  values clause is causing issues
-          ;;  [:values {?cytoloc 
-          ;;            [:np-terminology/SL-0086 :np-terminlogy/GO_0005737]}] 
+          ;;  SL and GO values for cytoplasm
+           [:values 
+            {?cytoloc 
+             [:np-terminology/SL-0086 :np-terminology/GO_0005737]}] 
            [?entry :np/isoform ?iso]
+           ;;  phophorylation
            [?iso  (cat :np/keyword :np/term) :np-terminology/KW-0597] ;phosphorylated
            [?iso :np/cellularComponent ?loc]
            [?loc (cat :np/term :np/childOf)  ?cytoloc]
            ;No negative localization evidence
-           [:filter (not (exists [[?loc :np/negativeEvidence ?negev]]))] ] 
-   })
+           [:filter (not (exists [[?loc :np/negativeEvidence ?negev]]))]]
+   :limit 10})
+
+
+;; ## NXQ2 
+;; Protens in the nucleus AND the cytoplasm
+
+(sq/query
+ :nextprot
+ `{:select-distinct [?entry]
+   :where [ [:values
+             {?cytoloc [:np-terminology/SL-0086 :np-terminology/GO_0005737]
+              ?nucloc [:np-terminology/SL-0191 :np-terminology/GO_0005634]}]
+           [?entry :np/isoform ?iso]
+           [?iso  (cat :np/keyword :np/term) :np-terminology/KW-0597] ;phosphorylated
+           [?iso :np/cellularComponent ?loc1]
+           [?iso :np/cellularComponent ?loc2]
+           [?loc1 (cat :np/term :np/childOf)  ?cytoloc]
+           [?loc2 (cat :np/term :np/childOf)  ?nucloc]
+           ;No negative localization evidence
+           [:filter (not (exists [[?loc1 :np/negativeEvidence ?negev]]))]
+           [:filter (not (exists [[?loc2 :np/negativeEvidence ?negev]]))]] 
+   :limit 10})
+
